1d627fb7cecb800e858961a5d05f2474d7a6d750
Rafael Miranda Almendaris <51010506+rafaelmirandaa@users.noreply.github.com>
2024-09-28 14:01:29 -0500

    Subiendo archivos de pruebas
    Subiendo archivos

---
+++ 13-DOM/js/07-scripts.js
@@ -0,0 +1,9 @@
+// const encabezado = document.querySelector('h1');
+
+// encabezado.style.backgroundColor = 'red';
+// encabezado.style.fontFamily = 'Arial ';
+// encabezado.style.textTransform = 'Uppercase';
+
+const card = document.querySelector('.card');
+card.classList.add('nueva_clase');
+console.log(card.classList);
---
+++ 13-DOM/js/08-scripts.js
@@ -0,0 +1,23 @@
+// const navegacion = document.querySelector('.navegacion');
+
+// console.log(navegacion);
+// console.log(navegacion.childNodes); //Los espacios en blanco son considerados elementos
+// console.log(navegacion.children); //muestran solo los elementos 
+
+
+
+// const card = document.querySelector('.card');
+// card.children[1].children[1].textContent = 'Modificando desde consola';
+
+// card.children[0].src = 'img/hacer3.jpg';
+// console.log(card.children[0]);
+
+
+//traversing del hijo al padre
+
+// console.log(card.parentElement);
+
+
+//console.log(card.nextElementSibling);
+
+
