4c758575e123ebf7c08d04ce5d80ad607c41e296
rafaelmirandaa <rafael.miranda.almendaris@gmail.com>
2025-01-14 23:03:25 -0500

    actualizacion

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -16,17 +16,44 @@ document.addEventListener('DOMContentLoaded', function (){
     function validar(e){
         if(e.target.value.trim() ===''){ //trim elimina los espacios vacios
             mostrarAlerta(`El Campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        }else{
-            console.log('Si hay algo');
-        };
+            return;
+        }
+
+        validarEmail(e.target.value);
+
+        limpiarAlerta(e.target.parentElement);
+
     }
    
     function mostrarAlerta(mensaje , referencia){
+        //Comprueba si ya existe una alerta
+        limpiarAlerta(referencia);
+
+
+
         const error = document.createElement('P');
         error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white' , 'text-center', 'p-2');
 
         referencia.appendChild(error);
     }
+
+
+    function limpiarAlerta(referencia){
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const resultado = regex.test(email);
+
+        console.log(resultado);
+
+    }
+
+
 
 });
